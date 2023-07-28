0603e4c148b5e4365f85ed2e7619d378e6e1ffa7
maryemjbeli <maryemjbeli1998@gmail.com>
2023-07-27 17:51:09 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,3 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints buffer
+ * @b: buffer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
 {
 	int o, j, i;
 
